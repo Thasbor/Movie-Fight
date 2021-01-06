5ff4cbf01a03c9e48a3596cbd9b17e9bb6fe313b
Jeff Goin <goin.jeffrey@gmail.com>
2021-01-06 11:30:32 -0800

    drop down working

--- index.js
+++ index.js
@@ -6,23 +6,44 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
          }
      });
+
+     if(response.data.Error){
+         return [];
+     }
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search For a Movie<b/></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <dive class="dropdown-content results"></div>
+        </div>    
+    </div>    
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown')
+const resultswrapper = document.querySelector('.results')
 
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
-    for(let movie of movies){
-        const div = document.createElement('div')
+    
 
-        div.innerHTML = `
+    for(let movie of movies){
+        const option = document.createElement('a')
+
+        dropdown.classList.add('is-active');
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
         <img src = "${movie.Poster}"\>
-        <h1>${movie.Title}<\h1>
+        ${movie.Title}
         `;
 
-        document.querySelector('#target').appendChild(div)
+        resultswrapper.appendChild(option)
 
     }
 }
